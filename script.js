c36305a1e13c0f15ee42d6935692e5dd38ad67ff
Christy Yu <christyyu@ucsb.edu>
2026-07-02 19:30:50 -0400

    i mean i guess

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+import Clarity from '@microsoft/clarity';
+const projectId = "xewy12j17q"
+Clarity.init(projectId);
+
 const menu = document.querySelector(".menu");
 const menuIcon = document.querySelector(".menu_dark");
 let menuOpen = false;
